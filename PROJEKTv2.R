f8a96f607aed4dd5d4edb750d7f93cb9f7e38c06
Mateusz <matesto479@student.polsl.pl>
2016-05-17 23:31:26 +0200

    Add files via upload
    Still workin on Lilliefors normallity test, not sure if workin correctly rigth now.

--- PROJEKTv2.R
+++ PROJEKTv2.R
@@ -120,6 +120,8 @@ wydajnosc2.pozycyjny_wspolczynnik_zmiennosci_rozdzielczy <- poz_wspl_zmien(wydaj
 hist(wydajnosc1)
 hist(wydajnosc2)
 
+install.packages("nortest")
+library("nortest")
 
 przecietna_wydajnosc <- function(x)
 {
@@ -135,36 +137,59 @@ czy_wieksza_wydajnosc <- function(x1, x2)
 }
 tmp <- czy_wieksza_wydajnosc(wydajnosc1, wydajnosc2)
 
-lillieforsBiceps <- function (x, pvalue2) 
+lillieforsBiceps <- function (x) 
 {
-  DNAME <- deparse(substitute(x))
-  x <- sort(x[complete.cases(x)])
-  n <- length(x)
-  if (n < 5) 
-    stop("Wielkosc probki musi byc wieksza od 4")
-  p <- pnorm((x - mean(x))/sd(x))
-  Dplus <- max(seq(1:n)/n - p)
-  Dminus <- max(p - (seq(1:n) - 1)/n)
-  K <- max(Dplus, Dminus)
-  if (n <= 100) {
-    Kd <- K
-    nd <- n
-  }
-  else {
-    Kd <- K * ((n/100)^0.49)
-    nd <- 100
-  }
-  pvalue <- pvalue2
-  RVAL <- list(statistic = c(D = K), p.value = pvalue, method = "Test Kolmogorova-Lillieforsa", 
-               data.name = DNAME)
-  class(RVAL) <- "htest"
-  return(RVAL)
+    DNAME <- deparse(substitute(x))
+    x <- sort(x[complete.cases(x)])
+    n <- length(x)
+    if (n < 5) 
+      stop("Przykladowa probka musi byc wieksza od 4")
+    p <- pnorm((x - mean(x))/sd(x))
+    Dplus <- max(seq(1:n)/n - p)
+    Dminus <- max(p - (seq(1:n) - 1)/n)
+    K <- max(Dplus, Dminus)
+    if (n <= 100) {
+      Kd <- K
+      nd <- n
+    }
+    else {
+      Kd <- K * ((n/100)^0.49)
+      nd <- 100
+    }
+    pvalue <- exp(-7.01256 * Kd^2 * (nd + 2.78019) + 2.99587 * 
+                    Kd * sqrt(nd + 2.78019) - 0.122119 + 0.974598/sqrt(nd) + 
+                    1.67997/nd)
+    if (pvalue > 0.05) {
+      KK <- (sqrt(n) - 0.01 + 0.85/sqrt(n)) * K
+      if (KK <= 0.302) {
+        pvalue <- 1
+      }
+      else if (KK <= 0.5) {
+        pvalue <- 2.76773 - 19.828315 * KK + 80.709644 * 
+          KK^2 - 138.55152 * KK^3 + 81.218052 * KK^4
+      }
+      else if (KK <= 0.9) {
+        pvalue <- -4.901232 + 40.662806 * KK - 97.490286 * 
+          KK^2 + 94.029866 * KK^3 - 32.355711 * KK^4
+      }
+      else if (KK <= 1.31) {
+        pvalue <- 6.198765 - 19.558097 * KK + 23.186922 * 
+          KK^2 - 12.234627 * KK^3 + 2.423045 * KK^4
+      }
+      else {
+        pvalue <- 0
+      }
+    }
+    RVAL <- list(statistic = c(D = K), p.value = pvalue, method = "Test Kolmogorova-Lillieforsa", 
+                 data.name = DNAME)
+    class(RVAL) <- "htest"
+    return(RVAL)
 }
 
-lillieforsBiceps(wydajnosc1,0.05)
-lillieforsBiceps(wydajnosc2,0.95)
-wydajnosc1_kolm <- lillieforsBiceps(wydajnosc1,0.95)
-wydajnosc2_kolm <- lillieforsBiceps(wydajnosc2,0.95)
+lillieforsBiceps(wydajnosc1)
+lillieforsBiceps(wydajnosc2)
+wydajnosc1_kolm <- lillieforsBiceps(wydajnosc1)
+wydajnosc2_kolm <- lillieforsBiceps(wydajnosc2)
 
 odchylenie_std_wydajnosci <- function(x)
 {
